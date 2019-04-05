f484aac6c4a20983ad97532529a6d2972394c411
Alan <eagle870608@gmail.com>
2019-04-05 16:46:51 +0800

    script import fonst & style.css modify url (PPT into PT)

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,22 @@ import squirrel from '../images/squirrel.jpg';
 import '../css/normalize.css';
 import '../css/style.css';
 //fonts
-import "../fonts/PT-Sans.eot";
-import "../fonts/PT-Sans-Bold.eot";
-import "../fonts/PT-Sans-Italic.eot";
-import "../fonts/PT-Sans-BoldItalic.eot";
-import "../fonts/PT-Sans.woff";
-import "../fonts/PT-Sans-Bold.woff";
-import "../fonts/PT-Sans-Italic.woff";
-import "../fonts/PT-Sans-BoldItalic.woff";
-import "../fonts/PT-Sans.ttf";
-import "../fonts/PT-Sans-Bold.ttf";
-import "../fonts/PT-Sans-Italic.ttf";
-import "../fonts/PT-Sans-BoldItalic.ttf";
-import "../fonts/PT-Sans.svg";
-import "../fonts/PT-Sans-Bold.svg";
-import "../fonts/PT-Sans-Italic.svg";
-import "../fonts/PT-Sans-BoldItalic.svg";
+import "../css/fonts/PT-Sans.eot";
+import "../css/fonts/PT-Sans-Bold.eot";
+import "../css/fonts/PT-Sans-Italic.eot";
+import "../css/fonts/PT-Sans-BoldItalic.eot";
+import "../css/fonts/PT-Sans.woff";
+import "../css/fonts/PT-Sans-Bold.woff";
+import "../css/fonts/PT-Sans-Italic.woff";
+import "../css/fonts/PT-Sans-BoldItalic.woff";
+import "../css/fonts/PT-Sans.ttf";
+import "../css/fonts/PT-Sans-Bold.ttf";
+import "../css/fonts/PT-Sans-Italic.ttf";
+import "../css/fonts/PT-Sans-BoldItalic.ttf";
+import "../css/fonts/PT-Sans.svg";
+import "../css/fonts/PT-Sans-Bold.svg";
+import "../css/fonts/PT-Sans-Italic.svg";
+import "../css/fonts/PT-Sans-BoldItalic.svg";
 
 
 $('#image__bird').attr("src",bird);
