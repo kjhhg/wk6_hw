ce3e06079316a17032a140021b152dfcf2c6d4b4
Alan <eagle870608@gmail.com>
2019-04-05 11:17:13 +0800

    module img lodaer & html modify

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,31 @@ module.exports = {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js',
         publicPath: './dist/'
- },
- module:{
-	rules:[{
-	    test:/\.(js)$/,
-	    exclude:/(node_modules)/,
-	    use:{
-            loader:'babel-loader',
-            options:{
-                presets:['@babel/preset-env']
+    },
+    module:{
+        rules:[{
+            test:/\.(js)$/,
+            exclude:/(node_modules)/,
+            use:{
+                loader:'babel-loader',
+                options:{
+                    presets:['@babel/preset-env']
+                }
             }
-	    }
-	}]
+        },
+        {
+            test:/ \.(jpe?g|png|gif|svg)$/,
+            use:[
+                {
+                    loader:'url-loader',
+                    options:{
+                        limit:40000,
+                        outputPath:'./images'
+                    }
+                },
+                'image-webpack-loader'
+            ]
+        }]
     }
 
 }
