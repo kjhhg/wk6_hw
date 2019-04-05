a1a4f0a17d16f8a4fe63a351d5ed871973cef151
Alan <eagle870608@gmail.com>
2019-04-05 16:25:31 +0800

    script.js import css & fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 import $  from 'jquery' ;
+//images
 import bird from '../images/bird.jpg';
 import squirrel from '../images/squirrel.jpg';
+//css
+import '../css/normalize.css';
+import '../css/style.css';
+//fonts
+import "../fonts/PT-Sans.eot";
+import "../fonts/PT-Sans-Bold.eot";
+import "../fonts/PT-Sans-Italic.eot";
+import "../fonts/PT-Sans-BoldItalic.eot";
+import "../fonts/PT-Sans.woff";
+import "../fonts/PT-Sans-Bold.woff";
+import "../fonts/PT-Sans-Italic.woff";
+import "../fonts/PT-Sans-BoldItalic.woff";
+import "../fonts/PT-Sans.ttf";
+import "../fonts/PT-Sans-Bold.ttf";
+import "../fonts/PT-Sans-Italic.ttf";
+import "../fonts/PT-Sans-BoldItalic.ttf";
+import "../fonts/PT-Sans.svg";
+import "../fonts/PT-Sans-Bold.svg";
+import "../fonts/PT-Sans-Italic.svg";
+import "../fonts/PT-Sans-BoldItalic.svg";
+
 
 $('#image__bird').attr("src",bird);
 $('#image__squirrel').attr("src",squirrel);
